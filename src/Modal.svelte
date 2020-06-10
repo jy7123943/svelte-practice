07c35078f3a08b765aedab9f3b6a64ae79ac3968
juyeon <juyeonh@rainist.com>
2020-06-10 21:57:57 +0900

    Add modal css

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -3,13 +3,29 @@
 </script>
 
 {#if showModal}
-<div class="backdrop">
-  <div class="modal">
-    <p>Sign up for offers!</p>
+  <div class="backdrop">
+    <div class="modal">
+      <p>Sign up for offers!</p>
+    </div>
   </div>
-</div>
 {/if}
 
 <style>
+  .backdrop {
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    background: rgba(0, 0, 0, 0.8);
+  }
 
+  .modal {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;
+    text-align: center;
+    background: white;
+  }
 </style>
