2423bb4feb49dcb14b9bbd3bec6d4b035ced7575
juyeon <juyeonh@rainist.com>
2020-06-15 22:08:33 +0900

    Add checkbox in form

--- src/AddPersonForm.svelte
+++ src/AddPersonForm.svelte
@@ -2,9 +2,13 @@
   let name;
   let hairColor;
   let age;
+  // let fighting = false;
+  // let sneaking = false;
+  // let running = false;
+  let skills = [];
 
   const handleSubmit = () => {
-    console.log(name, hairColor, age);
+    console.log(name, hairColor, age, skills);
   };
 </script>
 
@@ -13,6 +17,25 @@
   <input type="text" placeholder="name" bind:value={name} />
   <input type="text" placeholder="hair color" bind:value={hairColor} />
   <input type="number" placeholder="age" bind:value={age} />
+  <h4>Skills:</h4>
+  <!-- <label>
+    <input type="checkbox" bind:checked={fighting} />fighting<br />
+  </label>
+  <label>
+    <input type="checkbox" bind:checked={sneaking} />sneaking<br />
+  </label>
+  <label>
+    <input type="checkbox"  bind:checked={running}/>running<br />
+  </label> -->
+  <label>
+    <input type="checkbox" bind:group={skills} value="fighting" />fighting<br />
+  </label>
+  <label>
+    <input type="checkbox" bind:group={skills} value="sneaking" />sneaking<br />
+  </label>
+  <label>
+    <input type="checkbox" bind:group={skills} value="running" />running<br />
+  </label>
   <button>Add Person</button>
 </form>
 
