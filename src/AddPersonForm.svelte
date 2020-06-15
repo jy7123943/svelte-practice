bff43da913e4b09c0470b2eed080e0a8533452b2
juyeon <juyeonh@rainist.com>
2020-06-15 22:00:17 +0900

    Add form in svelte app

---
+++ src/AddPersonForm.svelte
@@ -0,0 +1,19 @@
+<script>
+  let name;
+  let hairColor;
+  let age;
+
+  const handleSubmit = () => {
+    console.log(name, hairColor, age);
+  };
+</script>
+
+<h3>Add a new person</h3>
+<form on:submit|preventDefault={handleSubmit}>
+  <input type="text" placeholder="name" bind:value={name} />
+  <input type="text" placeholder="hair color" bind:value={hairColor} />
+  <input type="number" placeholder="age" bind:value={age} />
+  <button>Add Person</button>
+</form>
+
+<style></style>
--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Modal from './Modal.svelte';
+	import AddPersonForm from './AddPersonForm.svelte';
 
 	let showModal = false;
 
@@ -27,12 +28,7 @@
 </script>
 
 <Modal {showModal} on:click={toggleModal}>
-	<h3>Add a new person</h3>
-	<form>
-		<input type="text" placeholder="name" />
-		<input type="text" placeholder="hair color" />
-		<button>Add Person</button>
-	</form>
+	<AddPersonForm />
 	<!-- <div slot="title">
 		<h3>Add a new person</h3>
 	</div> -->
